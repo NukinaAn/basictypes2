6f7598c6682ce5f81da436fc75ccde0d7b5e12e9
NukinaAn <nukina2694@gmail.com>
2017-02-28 18:45:11 +0200

    Update Test6.java

--- src/main/java/uits/jv1614/basictypes2/Test6.java
+++ src/main/java/uits/jv1614/basictypes2/Test6.java
@@ -16,24 +16,25 @@ public class Test6 {
             triangle2(s);
      }
      
-         public static void triangle(int[][] arr){
-             
-         outer: for (int i=0; i<5; i++) {
-            for(int j=0; j<5; j++) {
+         public static void triangle(int[][] arr){ // начальный треугольник
+         
+             // оператор continue содержит метку, она указывает в каком из циклов мы хотим досрочно прекратить выполнение текущей итерации
+         outer: for (int i=0; i<5; i++) { // цикл для строки; 
+            for(int j=0; j<5; j++) {  // цикл для столбца
                 if(j > i) {
         System.out.println();
-            continue outer;
+            continue outer;  //  прерывает цикл подсчёта значений переменной j и продолжает его со следующей итерации цикла i
         }
-        System.out.print(" " + (i * j));
-        System.out.print(arr[i][j]+ " ");
+        System.out.print(" " + (i * j)); // печатает значение 
+        System.out.print(arr[i][j]+ " "); // печатает массив
         }
        }
    
    
-          System.out.println();
+          System.out.println(); // печатаем треугольник
         }
 
- 
+         // метод выполняет все тоже, что и придыдущий. Только меняем цикл отображае его симметрично по горизонтали
          public static void triangle1(int[][] arr){
           outer: for (int i=4; i>=0; i--) {
             for(int j=0; j<6; j++) {
@@ -49,6 +50,8 @@ public class Test6 {
    
           System.out.println();
         }
+         
+         //новый метод в котором по идеи должен был компилироваться треугольник с прямым углом в правом нижнем углу, но увы(((
              public static void triangle2(int[][] arr){
           for (int i=0; i<arr.length; i++) {
           int x = arr[arr.length - 1].length - arr[i].length;
